4bd619580aebb68da043f43f181aab360cbf3385
Josh Bowen <bowenjosh@google.com>
2017-01-30 12:24:08 -0700

    First working pass

--- main.go
+++ main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"golang.org/x/oauth2/google"
+	"google.golang.org/api/cloudbuild/v1"
+	"context"
+)
+
 
 func main() {
-	fmt.Println("Hello Go")
+
+	ctx := context.Background()
+
+	//Get our client
+	client, err := google.DefaultClient(ctx, cloudbuild.CloudPlatformScope)
+	if err != nil {
+		fmt.Printf("Error getting client: %v\n", err)
+	}
+
+	//Get our service
+	buildService, err := cloudbuild.New(client)
+	if err != nil {
+		fmt.Printf("Error getting builder: %v\n", err)
+	}
+
+
+	listOp := buildService.Projects.Builds.List("cloud-container-demo")
+	resp, err := listOp.Do()
+	if err != nil {
+		fmt.Printf("Error running list operation: %v\n", err)
+	}
+
+	fmt.Printf("Got Response:\n%v", resp)
+
 }
